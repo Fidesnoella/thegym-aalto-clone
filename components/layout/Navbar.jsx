135ba6b766facf9d4b22d741dcd7b5e79f5a6d8c
Fidesnoella <fniragena@gmail.com>
2023-02-27 19:04:42 +0200

    ft: make footer responsive

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -5,7 +5,7 @@ import lock from "../../public/assets/lock.svg"
 
 export default function Navbar() {
     return (
-        <div className='grid grid-cols-2 items-center border-black border-b-2 bg-white text-black pl-20 pr-14'>
+        <div className='grid grid-cols-2 items-center border-black border-b-2 bg-white text-black px-3 md:pl-20 md:pr-14 py-2 md:py-0'>
             <Image src={Logo} className='w-20 md:w-24' />
             <div className='flex gap-8 items-center'>
                 <ul className='hidden font-normal text-[#2d2d2d] text-base leading-[19px] md:flex items-center gap-8 py-[35px]'>
@@ -13,11 +13,13 @@ export default function Navbar() {
                     <li>Support us</li>
                     <li>EN</li>
                 </ul>
-                <ul>
+                <ul className='flex font-normal text-[#2d2d2d] gap-8'>
                     <li>Search</li>
                     <li>Menu</li>
                 </ul>
-                <Button style={"text-white"}>Log in</Button>
+                <div className='hidden md:block'>
+                    <Button style={"text-white"}>Log in</Button>
+                </div>
             </div>
         </div>
     );
